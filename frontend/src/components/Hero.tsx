6ceb93ffd1a32892980bbb43ee44fe955430b145
mickeysrishti <Mukeshpathak345@gmail.com>
2023-12-29 16:49:50 +0530

    first commit

---
+++ frontend/src/components/Hero.tsx
@@ -0,0 +1,20 @@
+const Hero = () => {
+    return (
+        <div className="bg-blue-800 pb-16">
+            <div className="container mx-auto flex flex-col gap-2 pt-5">
+                <h1 className="text-5xl text-white font-bold">
+                    Find your next stay!
+                </h1>
+                <p className="text-1xl text-white pt-4">
+                    Search low price on hotels for your dream vacations.
+                </p>
+
+            </div>
+
+            
+        </div>
+    )
+}
+
+
+export default Hero;
